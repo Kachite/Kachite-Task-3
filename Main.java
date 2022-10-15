18f7d24080280e7c2620457ccabd0dbb8aa67cdd
kachi <victor.uzomba@yahoo.com>
2022-10-16 00:12:36 +0300

    This is the Task 3

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 
 public class Main {
-    public static final Line L1 = new Line(-3, 2, -1);
-    public static final HorizontalParabola P1 = new HorizontalParabola(3, -2, -1);
-    public static final Circle C1 = new Circle(2, -2, 3);
-
+    public static final Line L1 = new Line(0, 3, 0.5);
+    public static final LeftParabola P1 = new LeftParabola(-1, -1, -0.5);
+    public static final RightParabola P2 = new RightParabola(-5, 2, 0.5);
+    public static final Rectangle R1 = new Rectangle(-2,0,2,-6);
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter x coordinate: ");
@@ -19,19 +19,18 @@ public class Main {
     }
 
     public static SimpleColor getColor(double x, double y) {
-        if ((P1.isBelowParabola(x, y) && C1.isWithinCircle(x, y))
-                || (L1.isPointAboveLine(x, y) && C1.isWithinCircle(x, y))) {
-            return SimpleColor.YELLOW;
-        }
-        if (L1.isPointAboveLine(x, y) && !C1.isWithinCircle(x, y) && !P1.isBelowParabola(x, y)) {
+        if ((!L1.isPointAboveLine(x, y) && !P1.isLeftOfParabola(x, y) && !R1.isWithinRectangle(x, y) && !P2.isRightOfParabola(x, y))
+        || (R1.isWithinRectangle(x, y) && !P1.isLeftOfParabola(x, y) && P2.isRightOfParabola(x, y))){
             return SimpleColor.ORANGE;
         }
-        if ((!L1.isPointAboveLine(x, y) && C1.isWithinCircle(x, y) && !P1.isBelowParabola(x, y))
-                || (!C1.isWithinCircle(x, y) && P1.isBelowParabola(x, y))
-                || (x > 5 && !L1.isPointAboveLine(x, y))) {
-            return SimpleColor.BLUE;
+        if (P1.isLeftOfParabola(x,y) && !P2.isRightOfParabola(x,y) && !L1.isPointAboveLine(x,y)){
+            return SimpleColor.GRAY;
         }
-        return SimpleColor.GRAY;
-    }
+        if ((L1.isPointAboveLine(x,y) && !P1.isLeftOfParabola(x,y) && !P2.isRightOfParabola(x,y))
+                || (L1.isPointAboveLine(x,y) && P1.isLeftOfParabola(x,y) && P2.isRightOfParabola(x,y))) {
+            return SimpleColor.GREEN;
+        }
+        return SimpleColor.YELLOW;
+   }
 }
 
